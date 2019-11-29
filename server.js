97fff70feb5dcc30bdd7bb25edd236cfee7bd74d
Ankit <salian.ankit@tcs.com>
2019-11-29 06:28:54 +0530

    Adding generic route

--- server.js
+++ server.js
@@ -1,10 +1,45 @@
 const express = require('express');
-const dontenv = require('dotenv');
+const dotenv = require('dotenv');
 
 //Load enviromnent variables
 dotenv.config({ path: './config/config.env'});
 
 const app = express();
+
+app.get('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json({
+        success: true,
+        msg: `View all bootcamps`
+    })
+});
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({
+        success: true,
+        msg: `Get bootcamp ${req.params.id}`
+    })
+})
+
+app.post('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json({
+        success: true,
+        msg: `Create new bootcamp`
+    })
+})
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({
+        success: true,
+        msg: `Updated bootcamp ${req.params.id}`
+    })
+})
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({
+        success: true,
+        msg: `Deleted bootcamp ${req.params.id}`
+    })
+})
 
 const PORT = process.env.PORT || 5000;
 

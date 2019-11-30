51f35604015c89a29dc22a63911afae4bba53f85
Ankit <salian.ankit@tcs.com>
2019-11-30 18:17:52 +0530

    Moved ErrorResponse to errorHandler to return error dynamically for POST methods

--- middleware/error.js
+++ middleware/error.js
@@ -6,12 +6,24 @@ const errorHandler = (err, req, res, next) => {
     error.message = err.message;
 
     //Log to console
-    console.log(err.stack.red);
+    console.log(err);
     
     //Mongoose bad ObjectIds
     if(err.name === 'CastError'){
         const message = `Resource not found with id of ${err.value}`;
         error = new ErrorResponse(message, 404);
+    }
+
+    //Mongoose duplicate keys
+    if(err.code === 11000){
+        const message = 'Duplicate field value entered';
+        error = new ErrorResponse(message, 400);
+    }
+
+    //Mongoose validation errors
+    if(err.name === 'ValidationError'){
+        const message = Object.values(err.errors).map(val => val.message);
+        error = new ErrorResponse(message, 400)
     }
     
     res.status(error.statuscode || 500).json({

937a250b9e7193e7d94761b66eb33e6742c3e61d
Ankit <salian.ankit@tcs.com>
2019-11-30 12:30:31 +0530

    Integrated with mongoose and created very first model Bootcamp

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-//Route files
-const bootcamps = require('./routes/bootcamp');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 //Load enviromnent variables
 dotenv.config({ path: './config/config.env'});
 
+//Connect to database
+connectDB();
+
+//Route files
+const bootcamps = require('./routes/bootcamp');
 const app = express();
 
 if(process.env.NODE_ENV === 'development'){
@@ -19,7 +23,14 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
     PORT, 
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
 );
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    //Close servers and exit process
+    server.close(() => process.exit(1));
+})

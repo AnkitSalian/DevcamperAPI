8e6b727320884194f01f70ac8fa9813ec0a5ebf0
Ankit <salian.ankit@tcs.com>
2019-12-03 06:24:46 +0530

    Created a new Schema Course and added a seeder to import and destroy data against it

--- seeder.js
+++ seeder.js
@@ -9,6 +9,7 @@ dotenv.config({path: './config/config.env'});
 
 //Load models
 const Bootcamp = require('./models/Bootcamps');
+const Course = require('./models/Course');
 
 //Connect to DB
 mongoose.connect(process.env.MONGO_URI , {
@@ -20,12 +21,13 @@ mongoose.connect(process.env.MONGO_URI , {
 
 //Read JSON files
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`,'utf-8'));
+const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`,'utf-8'));
 
 //Import into DB
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps);
-
+        await Course.create(courses);
         console.log('Data Imported...'.green.inverse);
         process.exit();
     } catch (error) {
@@ -38,7 +40,7 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Bootcamp.deleteMany();
-
+        await Course.deleteMany();
         console.log('Data Destroyed...'.red.inverse);
         process.exit();
     } catch (error) {

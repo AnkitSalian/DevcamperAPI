3922d1dd08eb9775f8506fa67fe1d684a799cd31
Ankit <salian.ankit@tcs.com>
2019-12-07 12:01:31 +0530

    Configured method to calculate average cost of tuition fees per bootcamp

--- models/Course.js
+++ models/Course.js
@@ -38,4 +38,40 @@ const CourseSchema = new mongoose.Schema({
     }
 })
 
+//Static methods to get average of course tuition
+CourseSchema.statics.getAverageCost = async function(bootcampId) {
+    console.log('Calculating average cost'.blue);
+    const obj = await this.aggregate([
+        {
+            $match: {bootcamp: bootcampId}
+        },
+        {
+            $group: {
+                _id: '$bootcamp',
+                averageCost: { $avg: '$tuition'}
+            }
+        }
+    ]);
+    
+    try {
+        await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+            averageCost: Math.ceil(obj[0].averageCost / 10) * 10
+        })
+    } catch (error) {
+        console.error(error);
+        
+    }
+    
+}
+
+//Call averageCost after save
+CourseSchema.post('save', function(){
+    this.constructor.getAverageCost(this.bootcamp);
+})
+
+//Call averageCost before remove
+CourseSchema.pre('remove', function(){
+    this.constructor.getAverageCost(this.bootcamp);
+})
+
 module.exports = mongoose.model('Course', CourseSchema);
--- seeder.js
+++ seeder.js
@@ -28,7 +28,7 @@ const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`,'ut
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps);
-        await Course.create(courses);
+        //await Course.create(courses);
         console.log('Data Imported...'.green.inverse);
         process.exit();
     } catch (error) {

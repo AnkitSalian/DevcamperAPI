888135a923dfa2be04170b6814f028392ac55482
Ankit <salian.ankit@tcs.com>
2019-12-10 05:23:50 +0530

    Forgot password integration

--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const brcypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
@@ -35,6 +36,9 @@ const UserSchema = new mongoose.Schema({
 
 //Encrypt password using brycpt
 UserSchema.pre('save', async function (next) {
+    if(!this.isModified('password')){
+        next();
+    }
     const salt = await brcypt.genSalt(10);
     this.password = await brcypt.hash(this.password, salt);
 })
@@ -51,5 +55,18 @@ UserSchema.methods.matchPassword = async function(enteredPassword) {
     return await brcypt.compare(enteredPassword, this.password);
 }
 
+//Generate and hash password token
+UserSchema.methods.getResetPasswordToken = function() {
+    //Generate Token
+    const resetToken = crypto.randomBytes(20).toString('hex');
+
+    //Hash token and set to resetPasswordToken field
+    this.resetPasswordToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+
+    //Set expire
+    this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+
+    return resetToken;
+} 
 
 module.exports = mongoose.model('User', UserSchema);
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {register, login, getMe} = require('../controllers/auth');
+const {register, login, getMe, forgotPassword} = require('../controllers/auth');
 
 const router = express.Router();
 
@@ -10,5 +10,7 @@ router.post('/register', register);
 router.post('/login', login);
 
 router.get('/me', protect, getMe);
+
+router.post('/forgotpassword', forgotPassword);
 
 module.exports = router;

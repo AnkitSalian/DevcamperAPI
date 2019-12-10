50d437059b5e7503de07433f3f31d9fa447c7010
Ankit <salian.ankit@tcs.com>
2019-12-10 06:11:49 +0530

    Integrated with mailtrap to send mails through nodemailer for reset password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const ErrorResponse = require('../utils/errorResponse');
 const User = require('../models/User');
+const sendEmail = require('../utils/sendEmail');
 const asyncHandler = require('../middleware/async');
 
 // @desc     Register user
@@ -80,10 +81,36 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
 
     await user.save({validateBeforeSave: false});
 
-    res.status(200).json({
-        success: true,
-        data: user
-    })
+    //Create reset URL
+    const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/resetpassword/${resetToken}`;
+
+    const message = `You are receiving this email because you (or someone else) has requested the reset of a password. Please make a PUT request to: \n\n ${resetUrl}`;
+
+    try {
+        await sendEmail({
+            email: user.email,
+            subject: 'Password Reset Token',
+            message
+        });
+
+        res.status(200).json({
+            success: true,
+            data: 'Email Sent'
+        })
+    } catch (error) {
+        console.log(error);
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpire = undefined;
+
+        await user.save({validateBeforeSave: false});
+
+        return next(new ErrorResponse('Email could not be sent', 500));
+    }
+
+    // res.status(200).json({
+    //     success: true,
+    //     data: user
+    // })
 })
 
 //Get token from model, create cookie and send response
